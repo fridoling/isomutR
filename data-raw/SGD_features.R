f41507499444d40db195b6ec8846d338ea39e398
fridoling <fridoling@hotmail.com>
2021-08-18 10:52:23 +0200

    add remove_features

--- data-raw/SGD_features.R
+++ data-raw/SGD_features.R
@@ -4,6 +4,7 @@ SGD_features_file <- system.file(
   "SGD_features.txt",
   package = "isomutR"
 )
+## load data and create GRanges object
 SGD_features_df <- read.table(SGD_features_file, header = TRUE, sep = "\t")
 SGD_features_df <- SGD_features_df[!SGD_features_df$chr %in% c("", "2-micron"),]
 SGD_features_df <- SGD_features_df[!is.na(SGD_features_df$start),]
@@ -14,5 +15,15 @@ SGD_end <- pmax(SGD_features_df$start, SGD_features_df$end)
 SGD_features <- GenomicRanges::GRanges(SGD_chr,
                                        IRanges::IRanges(SGD_start, SGD_end),
                                        )
+## add metadata
 GenomicRanges::values(SGD_features) <- SGD_features_df[,1:7]
+
+## filter for features to be removed
+SGD_features <- SGD_features[
+  grepl('centromere', SGD_features$FeatureType) |
+  grepl('telomere', SGD_features$FeatureType) |
+  grepl('telomeric',SGD_features$FeatureType) |
+  grepl('LTR', SGD_features$FeatureType) |
+  grepl('repeat', SGD_features$FeatureType)
+]
 usethis::use_data(SGD_features, overwrite = TRUE)
