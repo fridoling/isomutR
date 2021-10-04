a40645957f78967acaa84f9590ca3910832877c7
fridoling <fridoling@hotmail.com>
2021-10-04 17:57:06 +0200

    change data

--- data-raw/gtf_scer.R
+++ data-raw/gtf_scer.R
@@ -1,8 +1,9 @@
 ## code to prepare `gtf_scer` dataset goes here
 
-gtf_scer <- system.file(
+gtf_scer_path <- system.file(
   "extdata",
   "Saccharomyces_cerevisiae.R64-1-1.102.gtf",
   package = "isomutR"
 )
+gtf_scer <- rtracklayer::import(gtf_scer_path)
 usethis::use_data(gtf_scer, overwrite = TRUE)
